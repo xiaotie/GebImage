ca84bbb75d4d7ae2a5812a15d0327dbc6c0cb9a1
xiaotie <xiaotie@geblab.com>
2013-11-04 09:51:40 +0800

    生成 ImageRgbSingle96 模板

--- src/Geb.Image/UnmanagedImage/ImageRgbSingle96.cs
+++ src/Geb.Image/UnmanagedImage/ImageRgbSingle96.cs
@@ -1,12 +1,66 @@
-﻿using System;
+﻿/*************************************************************************
+ *  Copyright (c) 2013 Hu Fei(xiaotie@geblab.com; geblab, www.geblab.com)
+ ************************************************************************/
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
 
 namespace Geb.Image
 {
     public partial struct RgbSingle96
     {
         public Single Blue,Red,Green;
+
+        public RgbSingle96(int red, int green, int blue)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+        }
+
+        public RgbSingle96(byte red, byte green, byte blue)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+        }
+    }
+
+    public partial class ImageRgbSingle96 : IDisposable
+    {
+        public const int ChannelCount = 3;
+
+        #region Image <-> Bitmap 所需的方法
+
+        private unsafe void Copy(Rgb24* from, void* to, int length)
+        {
+            throw new NotImplementedException();
+        }
+
+        private unsafe void Copy(Argb32* from, void* to, int length)
+        {
+            throw new NotImplementedException();
+        }
+
+        private unsafe void Copy(byte* from, void* to, int length)
+        {
+            throw new NotImplementedException();
+        }
+
+        private System.Drawing.Imaging.PixelFormat GetOutputBitmapPixelFormat()
+        {
+            throw new NotImplementedException();
+        }
+
+        private unsafe void ToBitmapCore(byte* src, byte* dst, int width)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
     }
 }
---
+++ src/Geb.Image/UnmanagedImage/Templates/_ImageRgbSingle96.cs
@@ -0,0 +1,36 @@
+﻿/*************************************************************************
+ *  Copyright (c) 2013 Hu Fei(xiaotie@geblab.com; geblab, www.geblab.com)
+ ************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace Geb.Image
+{
+    using TPixel = RgbSingle96;
+    using TChannel = System.Single;
+    using TCache = System.Single;
+    using TKernel = System.Single;
+    using TImage = Geb.Image.ImageRgbSingle96;
+
+    public static partial class ImageRgbSingle96ClassHelper
+    {
+        #region include "__ImageClassHelper_Template.cs"
+        #endregion
+    }
+
+    public partial class ImageRgbSingle96
+    {
+        #region include "__Image_Template.cs" [Image_Template -> ImageRgbSingle96]
+        #endregion
+    }
+
+    public partial struct RgbSingle96
+    {
+        #region include "__Pixel_Template.cs"
+        #endregion
+    }
+}
