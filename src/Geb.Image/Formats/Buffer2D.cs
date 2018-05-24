bd9be31ece2f55c5f6504b998b92f4fe81e68a19
DESKTOP-KUE4PHB\hufei <hufei@peredoc.com>
2018-05-24 10:07:56 +0800

    添加了jpg的关于代码

--- src/Geb.Image/Formats/Buffer2D.cs
+++ src/Geb.Image/Formats/Buffer2D.cs
@@ -74,8 +74,8 @@ namespace Geb.Image.Formats
         /// <param name="b">The second buffer</param>
         public static void SwapContents(Buffer2D<T> a, Buffer2D<T> b)
         {
-            Size aSize = a.Size();
-            Size bSize = b.Size();
+            System.Drawing.Size aSize = a.Size();
+            System.Drawing.Size bSize = b.Size();
 
             IBuffer<T> temp = a.Buffer;
             a.Buffer = b.Buffer;
@@ -87,90 +87,5 @@ namespace Geb.Image.Formats
             a.Width = bSize.Width;
             a.Height = bSize.Height;
         }
-    }
-
-    /// <summary>
-    /// Defines extension methods for <see cref="IBuffer2D{T}"/>.
-    /// </summary>
-    internal static class Buffer2DExtensions
-    {
-        /// <summary>
-        /// Gets a <see cref="Span{T}"/> to the row 'y' beginning from the pixel at 'x'.
-        /// </summary>
-        /// <param name="buffer">The buffer</param>
-        /// <param name="x">The x coordinate (position in the row)</param>
-        /// <param name="y">The y (row) coordinate</param>
-        /// <typeparam name="T">The element type</typeparam>
-        /// <returns>The <see cref="Span{T}"/></returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Span<T> GetRowSpan<T>(this IBuffer2D<T> buffer, int x, int y)
-            where T : struct
-        {
-            return buffer.Span.Slice((y * buffer.Width) + x, buffer.Width - x);
-        }
-
-        /// <summary>
-        /// Gets a <see cref="Span{T}"/> to the row 'y' beginning from the pixel at the first pixel on that row.
-        /// </summary>
-        /// <param name="buffer">The buffer</param>
-        /// <param name="y">The y (row) coordinate</param>
-        /// <typeparam name="T">The element type</typeparam>
-        /// <returns>The <see cref="Span{T}"/></returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Span<T> GetRowSpan<T>(this IBuffer2D<T> buffer, int y)
-            where T : struct
-        {
-            return buffer.Span.Slice(y * buffer.Width, buffer.Width);
-        }
-
-        /// <summary>
-        /// Returns the size of the buffer.
-        /// </summary>
-        /// <typeparam name="T">The element type</typeparam>
-        /// <param name="buffer">The <see cref="IBuffer2D{T}"/></param>
-        /// <returns>The <see cref="Size{T}"/> of the buffer</returns>
-        public static Size Size<T>(this IBuffer2D<T> buffer)
-            where T : struct
-        {
-            return new Size(buffer.Width, buffer.Height);
-        }
-
-        ///// <summary>
-        ///// Returns a <see cref="Rectangle"/> representing the full area of the buffer.
-        ///// </summary>
-        ///// <typeparam name="T">The element type</typeparam>
-        ///// <param name="buffer">The <see cref="IBuffer2D{T}"/></param>
-        ///// <returns>The <see cref="Rectangle"/></returns>
-        //public static Rectangle FullRectangle<T>(this IBuffer2D<T> buffer)
-        //    where T : struct
-        //{
-        //    return new Rectangle(0, 0, buffer.Width, buffer.Height);
-        //}
-
-        ///// <summary>
-        ///// Return a <see cref="BufferArea{T}"/> to the subarea represented by 'rectangle'
-        ///// </summary>
-        ///// <typeparam name="T">The element type</typeparam>
-        ///// <param name="buffer">The <see cref="IBuffer2D{T}"/></param>
-        ///// <param name="rectangle">The rectangle subarea</param>
-        ///// <returns>The <see cref="BufferArea{T}"/></returns>
-        //public static BufferArea<T> GetArea<T>(this IBuffer2D<T> buffer, Rectangle rectangle)
-        //    where T : struct => new BufferArea<T>(buffer, rectangle);
-
-        //public static BufferArea<T> GetArea<T>(this IBuffer2D<T> buffer, int x, int y, int width, int height)
-        //    where T : struct
-        //{
-        //    var rectangle = new Rectangle(x, y, width, height);
-        //    return new BufferArea<T>(buffer, rectangle);
-        //}
-
-        ///// <summary>
-        ///// Return a <see cref="BufferArea{T}"/> to the whole area of 'buffer'
-        ///// </summary>
-        ///// <typeparam name="T">The element type</typeparam>
-        ///// <param name="buffer">The <see cref="IBuffer2D{T}"/></param>
-        ///// <returns>The <see cref="BufferArea{T}"/></returns>
-        //public static BufferArea<T> GetArea<T>(this IBuffer2D<T> buffer)
-        //    where T : struct => new BufferArea<T>(buffer);
     }
 }
---
+++ src/Geb.Image/Formats/MathF.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Geb.Image.Formats
+{
+    public static class MathF
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Round(float val)
+        {
+            
+            return (float)Math.Round(val);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float Round(float val, MidpointRounding midpointRounding)
+        {
+            return (float)Math.Round(val, midpointRounding);
+        }
+    }
+}
