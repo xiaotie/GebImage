c43022168659d2b2d93dc5e7b943cc8ea35ed688
hufei <hufei@geblab.com>
2022-11-28 10:35:21 +0800

    添加  SkiaSharp 项目

--- pack/pack.bat
+++ pack/pack.bat
@@ -1,4 +1,5 @@
 nuget pack gebimage.nuspec -OutputDirectory ./package
-nuget pack gebimage_winform.nuspec  -OutputDirectory ./package
-nuget pack gebimage_edit.nuspec -OutputDirectory ./package
-nuget pack gebimage_analysis.nuspec -OutputDirectory ./package
+@REM nuget pack gebimage_winform.nuspec  -OutputDirectory ./package
+@REM nuget pack gebimage_edit.nuspec -OutputDirectory ./package
+@REM nuget pack gebimage_analysis.nuspec -OutputDirectory ./package
+nuget pack gebimage_skia.nuspec -OutputDirectory ./package
